7d252a86de967a9f13baf42a88ad8f7b7a557a52
Vasyl Kurtash <vasyl.kurtash@gmail.com>
2018-11-06 22:36:20 +0200

    POST & DELETE methods was added. The server is complete

--- routes/api/items.js
+++ routes/api/items.js
@@ -2,15 +2,38 @@ const express = require('express')
 const router = express.Router()
 
 // Item Model
-const Item = require('../../modules/Item')
+const Item = require('../../models/Item')
 
 //@route    GET api/items
 //@desc     Get All Items
 //@access   Public
 router.get('/', (req, res) => {
+    console.log("1111")
+    // Item.find({name: "Second object"})
     Item.find()
-        .sort({ data: -1 })
-        .then(items => res.json(items)) 
+        // .sort({ name: 1 })
+        .sort({ date: -1 })
+        .then(items => res.json(items))
+})
+
+// @route  POST api/items
+// @desc   Create An Item
+// @accexs Public
+router.post('/', (req, res) => {
+    const newItem = new Item({
+        name: req.body.name
+    })
+
+    newItem.save().then(item => res.json(item))
+})
+
+// @route    DELETE api/items/:id
+// @desc     Delete A Item
+// @access   Public
+router.delete('/:id', (req, res) => {
+    Item.findById(req.params.id)
+        .then(item => item.remove().then(() => res.json({ success: true })))
+        .catch(err => res.status(404).json({ success: false }))
 })
 
 module.exports = router
