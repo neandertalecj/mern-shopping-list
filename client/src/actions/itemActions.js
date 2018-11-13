f195be571f5f9a2655af996fc05a3588972c111f
Vasyl Kurtash <vasyl.kurtash@gmail.com>
2018-11-13 17:04:29 +0200

    Connecting the backend with "axios"

--- client/src/actions/itemActions.js
+++ client/src/actions/itemActions.js
@@ -1,20 +1,48 @@
-import { GET_ITEMS, ADD_ITEM, DELETE_ITEM } from './types'
+import axios from 'axios'
+import { GET_ITEMS, ADD_ITEM, DELETE_ITEM, ITEMS_LOADING } from './types'
 
-export const getItems = () => {
-    return {
-        type: GET_ITEMS
-    }
+export const getItems = () => dispatch => {
+    // return {
+    //     type: GET_ITEMS
+    // }
+    dispatch(setItemsLoading())
+    axios
+        .get('/api/items')
+        .then(res => 
+            dispatch({
+                type: GET_ITEMS,
+                payload: res.data
+            }))
 }
 
-export const deleteItem = id => {
-    return {
-        type: DELETE_ITEM,
-        payload: id
-    }
+export const deleteItem = id => dispatch => {
+    // return {
+    //     type: DELETE_ITEM,
+    //     payload: id
+    // }
+    axios.delete(`/api/items/${id}`).then( res =>
+        dispatch({
+            type: DELETE_ITEM,
+            payload: id
+        })
+    )
 }
-export const addItem = item => {
+export const addItem = item => dispatch => {
+    // return {
+    //     type: ADD_ITEM,
+    //     payload: item 
+    // }
+    axios
+        .post('/api/items', item)
+        .then(res => 
+            dispatch({
+                type: ADD_ITEM,
+                payload:res.data
+            }))
+}
+
+export const setItemsLoading = () => {
     return {
-        type: ADD_ITEM,
-        payload: item 
+        type: ITEMS_LOADING
     }
 }
